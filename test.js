8bcab45b2eafe374cf95978187792ed0dd378a11
Alex Kocharin <alex@kocharin.ru>
2014-03-30 18:06:49 +0000

    ...

--- test.js
+++ test.js
@@ -1,6 +1,22 @@
 
 if (process.argv[2] === 'travis_after') {
-	return console.log(process.env.hello)
+	var data = '{"state": "success","target_url": "https://travis-ci.org/rlidwka/travis-test/builds/21883335","description": "test... test... test...","context": "travis stuff"}'
+	var x = require('https').request({
+		path: '/repos/rlidwka/travis-test/statuses/'+process.env.TRAVIS_COMMIT,
+		host: 'api.github.com',
+		port: 443,
+		method: 'POST',
+		headers: {
+			Authorization: 'Basic '+process.env.GITHUB_AUTH,
+			'Content-length': data.length,
+			'User-Agent': 'node.js',
+			'Accept': '*/*',
+			'Content-type': 'application/json',
+		},
+	})
+	x.end(data)
+	x.on('response', function(x){x.pipe(process.stdout)})
+	return
 }
 
 module.exports = function(cb) {
